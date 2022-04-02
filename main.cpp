7803b721af0959f0b6770bc8a68ddce3f33456e3
1nspiration <luxiriouslive@gmail.com>
2022-04-03 02:03:56 +0400

    test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char **argv)
 {
+
     //QCoreApplication::addLibraryPath("C:/Qt/6.2.4/mingw_64/plugins"); //решение ошибки, которая не позволяет запустить приложение не из компилятора, используем это позже
     QApplication game(argc, argv);
 
